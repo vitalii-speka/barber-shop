056078658666e6eff99643b6291013ae861a6875
Vitalii Speka <spekava@gmail.com>
2020-08-08 20:07:28 +0300

    добавил скрипт.

---
+++ src/script.js
@@ -0,0 +1,33 @@
+$(document).ready(function () {
+	$('.slider').slick();
+});
+
+
+// {
+// 	arrows: true,
+// 	dots: true,
+// 	slidesToShow: 3,
+// 	autoplay: true,
+// 	// speed:1000,
+// 	autoplaySpeed: 800,
+// 	responsive: [{
+// 			breakpoint: 768,
+// 			settings: {
+// 				slidesToShow: 2
+// 			}
+// 		},
+// 		{
+// 			breakpoint: 550,
+// 			settings: {
+// 				slidesToShow: 1
+// 			}
+// 		}
+// 	]
+// }
+// $('.fade').slick({
+// 	dots: true,
+// 	infinite: true,
+// 	speed: 500,
+// 	fade: true,
+// 	cssEase: 'linear'
+// });
