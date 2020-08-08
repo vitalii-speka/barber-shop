87f8d5979a884f336d461d152f14b5180538debc
Vitalii Speka <spekava@gmail.com>
2020-08-08 21:45:44 +0300

    Updates

--- script.75da7f30.js
+++ script.75da7f30.js
@@ -176,7 +176,7 @@ var parent = module.bundle.parent;
 if ((!parent || !parent.isParcelRequire) && typeof WebSocket !== 'undefined') {
   var hostname = "" || location.hostname;
   var protocol = location.protocol === 'https:' ? 'wss' : 'ws';
-  var ws = new WebSocket(protocol + '://' + hostname + ':' + "50289" + '/');
+  var ws = new WebSocket(protocol + '://' + hostname + ':' + "53603" + '/');
 
   ws.onmessage = function (event) {
     checkedAssets = {};
